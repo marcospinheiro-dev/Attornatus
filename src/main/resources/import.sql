1a34ff921db356cb72dc3e75537d8f5a68bebce6
marcospinheiro-dev <marcos.pinheiro.dev@gmail.com>
2023-02-19 07:52:37 -0300

    Correção método Post

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO tb_pessoa(data_Nascimento, nome) VALUES ('1966-02-28', 'Marcos');
+INSERT INTO tb_pessoa(data_Nascimento, nome) VALUES ('1994-10-07', 'Lucas');
+INSERT INTO tb_pessoa(data_Nascimento, nome) VALUES ('1997-09-02', 'Adelly');
+INSERT INTO tb_pessoa(data_Nascimento, nome) VALUES ('2000-04-07', 'Anna');
+
+
+INSERT INTO tb_endereco(CEP, logradouro, numero_Casa, id_pessoa) VALUES (53409790, 'Rua 31', 26, 1);
+INSERT INTO tb_endereco(CEP, logradouro, numero_Casa, id_pessoa) VALUES (53000100, 'Rua dos Oitizeiros', 2, 1);
+INSERT INTO tb_endereco(CEP, logradouro, numero_Casa, id_pessoa) VALUES (51234790, 'Campo Grande', 10, 3);
+INSERT INTO tb_endereco(CEP, logradouro, numero_Casa, id_pessoa) VALUES (58900100, 'Tamarineira', 8, 4);
+INSERT INTO tb_endereco(CEP, logradouro, numero_Casa, id_pessoa) VALUES (56909790, 'Rua das Acácias', 95, 2);
+
